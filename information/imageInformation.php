4896b95a251ed6ecb841efc4b5b6dc580414ffc3
kurniacf <kcf170202@gmail.com>
2021-11-20 23:19:25 +0700

    image information

--- information/imageInformation.php
+++ information/imageInformation.php
@@ -6,27 +6,18 @@ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization, X-Requested-With');
 
-if (!empty($_GET['id_information'])) {
+if (!empty($_GET['id_information']) && !empty($_POST['image_information'])) {
     $id_information = $_GET['id_information'];
+    $image_information = $_POST['image_information'];
 
-    if (empty($_FILES['image_information'])) {
-        http_response_code(404);
-        set_response(false, "Image Dont Empty!");
+    $query = "UPDATE information set image_information = '$image_information' WHERE id_information = '$id_information'";
+    $update = pg_query($connect, $query);
+
+    if ($update) {
+        set_response(true, "Image Update Information Success");
     } else {
-        $image_information = $_FILES['image_information']['name'];
-        $file = $_FILES['image_information']['tmp_name'];
-        $dir = "image_information/";
-        move_uploaded_file($file, $dir . $image_information);
-
-        $query = "UPDATE information set image_information = '$image_information' WHERE id_information = '$id_information'";
-        $insert = pg_query($connect, $query);
-
-        if ($insert) {
-            set_response(true, "Update Image Success");
-        } else {
-            http_response_code(400);
-            set_response(false, "Update Image Failed");
-        }
+        http_response_code(400);
+        set_response(false, "Image Update Information Failed!");
     }
 } else {
     http_response_code(400);
